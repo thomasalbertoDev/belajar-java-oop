ddd3272062c0ba00dfa229d7b36a7bd1b0d918f5
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-26 20:21:00 +0700

    learn: Belajar Constructor di Java OOP

--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,12 @@ class Person {
     String address;
     final String county = "Indonesia"; // Data tidak bisa diubah lagi tapi bisa diakses
 
+    // Constructor dalam Java merupakan method yang pertama kali dijalankan saat sebuah object dibuat
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
     public void sayHello(String paramName){
         System.out.println("Hello " + paramName + ", My name is " + name);
     }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,9 +1,7 @@
 public class PersonApp {
     public static void main(String[] args) {
         // Membuat Object
-        var person = new Person();
-        person.name = "Thomas Alberto";
-        person.address = "Jl.Surya Suci";
+        var person = new Person("Thomas Alberto" , "Jl.Surya Suci");
 
         System.out.println(person.name);
         System.out.println(person.address);
