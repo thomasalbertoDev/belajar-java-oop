49330f2eade8bc1460d6f4a1223aaee7e9b8a094
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-26 19:56:56 +0700

    learn: Belajar Java OOP

---
+++ src/PersonApp.java
@@ -0,0 +1,12 @@
+public class PersonApp {
+    public static void main(String[] args) {
+        // Membuat Object
+        var person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+    }
+}
