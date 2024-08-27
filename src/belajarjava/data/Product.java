79be88fc840a50f0d45d3b0c7d8a309758da0bb2
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-27 22:22:45 +0700

    learn: Belajar Package di Java OOP

---
+++ src/belajarjava/data/Product.java
@@ -0,0 +1,11 @@
+package belajarjava.data;
+
+class Product {
+    String name;
+    Integer price;
+
+    Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+}
