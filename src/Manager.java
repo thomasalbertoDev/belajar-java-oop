cbd4c66f23ba4a0a80c5a987977875e586b55788
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-27 20:43:58 +0700

    learn: Belajar method Overriding di Java OOP

--- src/Manager.java
+++ src/Manager.java
@@ -3,6 +3,6 @@ public class Manager {
 
     public void sayHello(String name){
         // this.name fungsi nya mengkases properti name yang ada di class Person bukan parameter name dari fungsi sayHello
-        System.out.println("Hello " + name + ", My name is " + this.name);
+        System.out.println("Hello " + name + ", My name is " + this.name + " and I am Manager");
     }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,3 +1,8 @@
 public class VicePresident extends  Manager {
     
+    @Override
+    public void sayHello(String name){
+        // this.name fungsi nya mengkases properti name yang ada di class Person bukan parameter name dari fungsi sayHello
+        System.out.println("Hello " + name + ", My name is " + this.name + " and I am VP");
+    }
 }
