bbe3a8e4d6074e9e274a216f0f8cdfd103486f2e
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-29 16:18:57 +0700

    learn: Belajar equals method di Java OOP

---
+++ src/belajarjava/application/EqualsApp.java
@@ -0,0 +1,12 @@
+package belajarjava.application;
+
+public class EqualsApp {
+
+    public static void main(String[] args) {
+        String first = "Thomas";
+        String second = "Thomas";
+
+        // Equest method digunakan untuk membandingkan isi dari object nya, yang dimana dikode ini object yang dibandingkan adalah object first dan second
+        System.out.println(first.equals(second));
+    }
+}
