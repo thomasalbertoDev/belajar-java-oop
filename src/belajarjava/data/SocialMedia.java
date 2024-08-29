aee17f90b80534a022266a48ec6a737d168dbc4d
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-29 16:37:00 +0700

    learn: Belajar Final Method di Java OOP

--- src/belajarjava/data/SocialMedia.java
+++ src/belajarjava/data/SocialMedia.java
@@ -5,9 +5,16 @@ class SocialMedia {
     String name;
 }
 
-final class Facebook extends SocialMedia {
+class Facebook extends SocialMedia {
+
+    final void (String username, String password) {
+    }
 }
 
 // Error karena class Facebook sudah menggunakan keyword final yang dimana keyword final berarti class tersebut tidak bisa diwariskan lagi
 // class FakeFacebook extends Facebook {
+
+//     // Error karena method sudah memiliki keyword final
+//     void (String username, String password) {
+//     }
 // }
