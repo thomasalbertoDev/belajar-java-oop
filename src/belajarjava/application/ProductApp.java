3d3d6255a44685e6fd7ed69593385224c0fed67e
thomasalbertoDev <thomasalberto456@gmail.com>
2024-08-29 16:23:02 +0700

    learn: Belajar hash code di Java OOP

--- src/belajarjava/application/ProductApp.java
+++ src/belajarjava/application/ProductApp.java
@@ -6,9 +6,14 @@ public class ProductApp {
 
     public static void main(String[] args) {
         Product product = new Product("Indomie", 1000);
+        Product product2 = new Product("Sosis", 5000);
         System.out.println(product.name);
         System.out.println(product.price);
 
         System.out.println(product);
+
+        // HashCode method merupakan method yang membandingkan isi dari object tapi hanya object yang memiliki tipe data integer
+        System.out.println(product.hashCode() == product2.hashCode());
+
     }
 }
